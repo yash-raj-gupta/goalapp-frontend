da120055d0e5aa9de53599bc34b597ed8cecc2ed
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-02-10 23:06:31 +0530

    connected to backend

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios'
 
-const API_URL = '/api/users/'
+const API_URL = 'https://goal-backend-apis.onrender.com/api/users/'
 
 //Register user
 const register = async (userData) => {
