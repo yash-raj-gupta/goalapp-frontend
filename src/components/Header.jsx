a314f50a749f942f926418debb5361f34f0aadab
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-03-01 16:26:57 +0530

    updated logo and name of the website

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
   return (
    <header className='header'>
     <div className="logo">
-        <Link to='/'>GoalSetter</Link>
+        <Link to='/'>TargetTracer</Link>
     </div>
     <ul>
         {user ? (
